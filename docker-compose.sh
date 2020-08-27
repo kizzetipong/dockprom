4d6e6395ab7f906f417a25760ee85f282dbc8889
Kittipong Boonyapisomparn <kizzetipong@gmail.com>
2020-08-27 11:49:32 +0700

    replace DOCKER_HOST to HOST_IP since it's used by docker

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,3 +1,3 @@
-DOCKER_HOST=`ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+'`
-echo "DOCKER_HOST is set as ${DOCKER_HOST}"
-export DOCKER_HOST=$DOCKER_HOST && docker-compose up -d
+HOST_IP=`ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+'`
+echo "HOST_IP is set as ${HOST_IP}"
+export HOST_IP=$HOST_IP && docker-compose up -d
